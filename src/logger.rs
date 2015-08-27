6c2cc7c7532428c40f2134b8161c72c3672eb70a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-08-27 02:19:08 +0200

    Improve logger: Add file logging and make stdout logging optional

--- src/logger.rs
+++ src/logger.rs
@@ -9,6 +9,9 @@ use std::path::Path;
 use std::fs;
 use std::io;
 use term::{self, ToStyle};
+use std::io::Write;
+use std::sync::Mutex;
+use std::ops::DerefMut;
 
 /// Returns a builder that can enable the logger globally.
 pub fn with_loglevel(lvl: LogLevelFilter) -> Builder<'static> {
@@ -72,7 +75,7 @@ impl<'a> Builder<'a> {
             filter.set(self.lvl);
             Box::new(Logger {
                 level_filter: filter,
-                logfile: file,
+                logfile: file.map(|f| Mutex::new(f)),
                 stdout: self.stdout,
             })
         }).map_err(|_| io::Error::new(
@@ -87,7 +90,7 @@ impl<'a> Builder<'a> {
 /// Use macros and functions of the `log` crate.
 struct Logger {
     level_filter: MaxLogLevelFilter,
-    logfile: Option<fs::File>,
+    logfile: Option<Mutex<fs::File>>,
     stdout: bool,
 }
 
@@ -102,25 +105,46 @@ impl Log for Logger {
             return;
         }
 
-        // Prepare module and file path (remove unnecessary parts)
+        // Prepare module path (remove crate name & add "::" if missing)
         let pos = record.target().find("::");
         let mod_path = match pos {
             None => "::",
             Some(pos) => &record.target()[pos ..],
         };
 
-        // Ignore the leading 'src/'
-        let file = &record.location().file()[4 ..];
+        // Ignore the leading 'src/' in the file path
+        let src_file = &record.location().file()[4 ..];
 
-        let (lvl_col, msg_col) = get_colors(record.level());
+        // If a logfile is specified (file logging is enabled)
+        if let Some(ref file) = self.logfile {
+            // Aquire a lock on the file to log into file. We may unwrap here
+            // because it will just panic if a thread paniced before, while
+            // holding the lock. It's very unlikely (maybe even impossible)
+            // that the thread will panic during the `write!`. And if it
+            // happens we want to propagate the panic to all threads.
+            // We ignore the result of `write!`, because: What else should we
+            // do? ;)
+            let _ = write!(file.lock().unwrap().deref_mut(),
+                "[{level: <5}][{module} @ {file}:{line}]> {msg}",
+                level = record.level(),
+                module = mod_path,
+                file = src_file,
+                line = record.location().line(),
+                msg = record.args());
+        }
 
-        println!("[{level: <5}][{module} @ {file}:{line}]{delim} {msg}",
-            level = lvl_col.paint(record.level()),
-            module = mod_path,
-            file = term::Color::Blue.paint(file),
-            line = record.location().line(),
-            delim = term::Color::White.paint("$"),
-            msg = msg_col.paint(record.args()));
+        // If logging to stdout is enabled
+        if self.stdout {
+            let (lvl_col, msg_col) = get_colors(record.level());
+
+            println!("[{level: <5}][{module} @ {file}:{line}]{delim} {msg}",
+                level = lvl_col.paint(record.level()),
+                module = mod_path,
+                file = term::Color::Blue.paint(src_file),
+                line = record.location().line(),
+                delim = term::Color::White.paint("$"),
+                msg = msg_col.paint(record.args()));
+        }
     }
 }
 
