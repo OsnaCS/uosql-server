9329e67c056849379986680cd37fb14952718728
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-08-31 01:22:25 +0200

    Add some sample code to ´parse` module

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,7 +1,18 @@
 //! This module contains functions and types for parsing SQL queries.
 //!
 
+// TODO: Remove this 
+#![allow(dead_code, unused_imports, unused_variables)]
+
 pub mod ast;
+pub mod token;
+pub mod lex;
+
+/// Represents a substring in the query string in byte indices.
+pub struct Span {
+    lo: u32,
+    hi: u32,
+}
 
 /// Main function of this module: Takes a sql query as string and returns
 /// the parsed AST.
