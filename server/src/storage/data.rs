8d56e9b60fb15b11301ebb59e551c43cfefe310e
hrieping <hrieping@cip51.informatik.uni-osnabrueck.de>
2015-09-11 11:47:40 +0200

    Implement basic seek functionality + next_row

--- server/src/storage/data.rs
+++ server/src/storage/data.rs
@@ -2,12 +2,13 @@ use std::vec::Vec;
 use super::Error;
 use super::types::SqlType;
 use super::types::Column;
-use std::io::{Write, Read, Seek};
+use std::io::{Write, Read, Seek, SeekFrom};
 
 #[derive(Debug)]
 pub struct Rows <B: Write + Read + Seek> {
     data_src: B,
     columns: Vec<Column>,
+    columns_size: u64,
 }
 
 /// Represents the lines read from file.
@@ -15,25 +16,55 @@ impl<B: Write + Read + Seek> Rows <B> {
 
     pub fn new(data_src: B, columns: &[Column]) -> Rows<B> {
         Rows { data_src: data_src,
-               columns: columns.to_vec() }
+               columns: columns.to_vec(),
+               columns_size: Self::get_columns_size(columns)}
     }
 
-    /// reads the current row and writes the data into target_buf
-    /// and moves the cursor to the beginnig of the next line which is not marked
-    /// as deleted
-    /// returns the bytes read
-    pub fn read_row<W: Write>(&self, target_buf: &W) -> Result<u64, Error> {
-       //let mut reader = (&mut self.data_src).take(10);
-       Ok(0)
+    fn get_columns_size(columns: &[Column]) -> u64 {
+        let mut size: u64 = 0;
+        for c in columns {
+            size += c.get_size() as u64;
+        }
+        size
+    }
+
+    /// reads the next row, which is not marked as deleted
+    /// and writes the data into target_buf
+    /// returns the bytes read or an Error otherwise.
+    pub fn next_row<W: Write>(&mut self, mut target_buf: &mut W)
+        -> Result<u64, Error>
+    {
+        let mut target_vec = Vec::<u8>::new();
+        let columns_size = self.columns_size;
+
+        let mut row_header: RowHeader = try!(self.read_header());
+
+        while row_header.is_deleted() {
+            self.skip_row();
+            row_header = try!(self.read_header());
+        }
+
+        try!(self.read_bytes(columns_size, &mut target_vec));
+        try!(target_buf.write_all(&target_vec));
+        Ok(target_vec.len() as u64)
+    }
+
+    pub fn skip_row(&mut self) -> Result<u64, Error> {
+        let columns_size = self.columns_size as i64;
+        self.set_pos(SeekFrom::Current(columns_size))
     }
 
     /// sets position before the first line
-    pub fn reset_pos(&mut self) {
+    pub fn reset_pos(&mut self) -> Result<u64, Error> {
+        self.set_pos(SeekFrom::Start(0))
     }
 
     /// sets position to offset
-    fn set_pos(&mut self, offset: u64) {
-
+    fn set_pos(&mut self, seek_from: SeekFrom) -> Result<u64, Error> {
+        match self.data_src.seek(seek_from) {
+            Ok(n) => Ok(n),
+            Err(e) => return Err(Error::Io(e))
+        }
     }
 
     /// reads the header of the current row
@@ -56,6 +87,8 @@ impl<B: Write + Read + Seek> Rows <B> {
         Err(Error::NoImplementation)
     }
 
+    /// reads the specified amount of bytes and writes them into target_buf
+    /// returns bytes_read or error if bytes_read != bytes_to_read
     fn read_bytes(&mut self, bytes_to_read: u64, mut target_buf: &mut Vec<u8>)
         -> Result<u64, Error>
     {
